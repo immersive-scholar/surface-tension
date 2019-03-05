69c6e7b93588c65459147ec953253255cccff8a6
Misha Rabinovich <mishawagon@gmail.com>
2019-03-05 15:30:53 -0500

    Looks like generating absolute path to data is not working consitently between local and remote, so making it relative which works on both. Renaming the viz file

--- index.php
+++ index.php
@@ -115,29 +115,7 @@
       return ($files[0]) ? $files[0] : false;
   }
   function load_viz($pathy){
-        echo '<p>'.$pathy.'</p>';
-        // base directory
-        $base_dir = __DIR__;
-
-        // server protocol
-        $protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
-
-        // domain name
-        $domain = $_SERVER['SERVER_NAME'];
-
-        // base url
-        $base_url = preg_replace("!^${doc_root}!", '', $base_dir);
-
-        // server port
-        $port = $_SERVER['SERVER_PORT'];
-        $disp_port = ($protocol == 'http' && $port == 80 || $protocol == 'https' && $port == 443) ? '' : ":$port";
-
-        // put em all together to get the complete base URL
-        //$url = "${protocol}://${domain}${disp_port}${base_url}";
-        $url = "${protocol}://${domain}${disp_port}";
-
-        echo "<a href='".$url.$pathy."'>".$url.$pathy."</a>"; // = http://example.com/path/directory
-
+        echo "<a href='".$pathy."'>".$pathy."</a>";
   }
  ?>
  </body>
