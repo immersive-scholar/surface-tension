dd301da1d369f4e43bd151c043a791c57dd25205
Misha Rabinovich <misha_rabinovich@uml.edu>
2019-06-01 13:47:45 -0400

    Gracefully handling USGS returning empty files

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
   <title>Surface Tension by Caitlin & Misha</title>
   <link href="favicon.ico" type="image/x-icon" rel="Favicon for Surface Tension" />
   <link rel="stylesheet" type="text/css" href="app/css/data-cacher.css">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Arvo|Asap');
+  </style>
   <!-- Global site tag (gtag.js) - Google Analytics -->
+
   <script async src="https://www.googletagmanager.com/gtag/js?id=UA-21385959-2"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
@@ -28,11 +32,13 @@
   $zoom = filter_input(INPUT_GET, 'zoom', FILTER_SANITIZE_URL);
   $map = filter_input(INPUT_GET, 'map', FILTER_SANITIZE_URL);
 
-  echo '<h1>USGS Streamflow Data Cacher</h1>';
+  echo '<h1>Surface Tension</h1>';
+  echo '';
+  echo '<h2><img id="loading" src="app/img/ajax_loader_blue_32.gif">Loading and caching USGS Streamflow data...</h2>';
   echo "\n";
   if( ! ini_get('date.timezone') ) {
     date_default_timezone_set('America/New_York');
-    echo '<h2><span class="bad">Server timezone not set </span>, using: '.date_default_timezone_get().'</h2>';
+    echo '<h2><span class="bad">Server timezone not set</span>, using: '.date_default_timezone_get().'</h2>';
   } else {
     echo '<h2>Server timezone set to '.ini_get('date.timezone').'</h2>';
   }
@@ -48,10 +54,10 @@
 
 
   $tempdata = $data_dir.'/temp.csv';
-  echo '<h2>Scanning for:</h2>';echo "\n";
-  echo '<h3>'.$filepath.'</h3>';echo "\n";
-  echo '<h3>Filesize: '.filesize($filesize_data_dir.$data_file_path).'</h3>';
-  if (filesize($filesize_data_dir.$data_file_path) == 0) echo "<span class='bad'><p><b>Latest file is empty!</b></p></span>";
+  echo '<h2>Scanning for:</h2>'.'<h3>'.$filepath.'</h3>';
+  echo '<h3> Filesize: '.filesize($filesize_data_dir.$data_file_path);
+  if (filesize($filesize_data_dir.$data_file_path) == 0) echo " <span class='bad'><b>Latest file is empty!</b></span>";
+  echo '</h3>';
   //does the file for this day exist?
   //then don't redownload
   //otherwise download it but if it fails use a previous one
@@ -65,12 +71,15 @@
     //*******************************************************************************************************************************************************************************
     load_viz($more_relative_filepath, $sorting, $sidebar, $zoom, $map);
   } else {
-    echo "<h3>No cached data for this day of it's empty.</h3>";echo "\n";
+
     //load the file from usgs.
     //if response code is 200, cache this file and load vis with this file as parameter
     //if response is not 200, don't cache the file and load vis with a previously cached file
 
     $url = "https://waterwatch.usgs.gov/webservices/realtime?format=csv";
+    //$url = "http://localhost:8888/app/data/realtime-streamflow-2019-06-01.csv";
+    //$url = "http://localhost:8888/app/data/realtime-streamflow-empty.csv";//testing empty file from USGS bug ;
+
     // This is where the file will be saved
     $fp = fopen(dirname(__FILE__).$tempdata, 'w+');
     // Replace spaces with %20 on the URL string
@@ -95,25 +104,25 @@
     // Close file handle
     fclose($fp);
 
-    echo '<h2>Downloading:</h2>';echo "\n";
-    echo '<h3>'.$url.'</h3>';echo "\n";
+    echo '<h2>Downloading temp file from:</h2>';echo "\n";
+    echo '<h3>'.$url.'</h3>';
 
     //show only if there is something to show e.g. 404 response
     if ($http_respond != "") {
-      echo '<h3><b>http_respond:</b> '.$http_respond.'</h3>';echo "\n";
+      echo '<h3><b>http_respond:</b> '.$http_respond.'</h3>';
     }
 
     if ($http_code != "200") echo "<span class='bad'>";
-    echo '<h3><b>http_code (200 is best):</b> '.$http_code.'</h3>';
+    echo '<h3> <b>http_code (200 is best): </b> '.$http_code.'</h3>';
     if ($http_code != "200") echo "</span>";
 
     $temp_file_size = filesize(dirname(__FILE__).$tempdata);
 
-    if ($temp_file_size == 0) echo "<span class='bad'><p><b>Temp file is empty!</b></p></span>";
+    if ($temp_file_size == 0) echo " <span class='bad'><h3><b>But temp file is empty!</b></h3></span>";
 
     if ($http_code == "200" && $temp_file_size > 0) {
       if (!copy(dirname(__FILE__).$tempdata, dirname(__FILE__).$filepath)){
-        echo "<p><span class='bad'>ERROR copying temp file into new cached file!</span></p>";
+        echo "<span class='bad'><h3><b>ERROR copying temp file into new cached file!</b></h3></span>";
       } else {
         //*******************************************************************************************************************************************************************************
         //copying worked. Load viz with new cached file
@@ -121,28 +130,28 @@
       }
     } else {
       //Can't load data, use previously cached file
-      echo '<p>Find last, non-empty cached file</p>';
-      echo "<ul>";
-      $last_cached_file = scan_dir(dirname(__FILE__).$data_dir);
-      echo $last_cached_file;
-      echo "</ul>";
-      //*******************************************************************************************************************************************************************************
+      echo '<h2>Finding last, non-empty cached file in '.dirname(__FILE__).$data_dir.'</h2>';
+
+      $last_cached_file = scan_dir(dirname(__FILE__).$data_dir, $filesize_data_dir);
+
       //Load viz with previously cached file
-      load_viz($more_relative_filepath.'/'.$last_cached_file, $sorting, $sidebar, $zoom, $map);
+      load_viz($relative_data_dir.'/'.$last_cached_file, $sorting, $sidebar, $zoom, $map);
     }
   }
   //scan_dir function based on https://stackoverflow.com/questions/11923235/scandir-to-sort-by-date-modified
   //modified to filter out non streamflow files and also only to return first file in array, i.e. last cached file
-  function scan_dir($dir) {
+  function scan_dir($dir, $filesize_data_dir) {
     $ignored = array('.', '..', '.svn', '.htaccess');
 
     $files = array();
-    foreach (scandir($dir) as $file) {
+    foreach ((scandir($dir, 1)) as $file) {
       if (in_array($file, $ignored)) continue;
-      if (filesize($file) == 0) continue;
-      if (strpos($file, 'streamflow') !== false) {
-        $files[$file] = filemtime($dir . '/' . $file);
-      }
+      $da_siz = filesize($filesize_data_dir.'/'.$file);
+      //echo '<h3>'.$da_siz.'</h3>';
+      if ($da_siz == 0) continue;
+      if (strpos($file, 'streamflow') === false) continue;
+
+      $files[$file] = filemtime($dir . '/' . $file);
     }
 
     arsort($files);
@@ -151,7 +160,10 @@
     return ($files[0]) ? $files[0] : false;
   }
   function load_viz($pathy, $sorting, $sidebar, $zoom, $map){
-    echo "<span class='good'>".$pathy."</span>";
+    echo "<span class='good'><h3>Found: ".$pathy."</h3></span>";
+    echo "<script>setTimeout(function(){document.getElementById('loading').style.display='none';},4000);  </script>";//turn off loading spinner
+    echo "<br><br><hr>";
+    echo "<h2>If you're not automatically redirected, <a href='app/viz.html?data=".$pathy."'>click here</a></h2>";
     $redir_js = "\n<script>\n
       setTimeout(function(){
       \n\t\twindow.location.href = 'app/viz.html?data=".$pathy;
@@ -162,6 +174,7 @@
     if ($map != "") { $redir_js .= "&map=".$map; }
 
       //"&sorting=".$sorting."&sidebar=".$sidebar."&zoom=".$zoom."&map=".$map
+
     $redir_js .= "';\n
       }, 3000)\n</script>\n";
     echo $redir_js;
